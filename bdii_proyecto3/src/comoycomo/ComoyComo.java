8af6164aa25a483ab5d89c09cf36684515fad8d2
DaFreddyIniguez <iniguez.devt@gmail.com>
2014-04-30 23:25:04 -0500

    Pestañas definidas, métodos para salir y ver documentación funcionan.

--- bdii_proyecto3/src/comoycomo/ComoyComo.java
+++ bdii_proyecto3/src/comoycomo/ComoyComo.java
@@ -1,17 +1,34 @@
 package comoycomo;
 
-import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Desktop;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+
 import javax.swing.*;
 
-public class ComoyComo extends JFrame{
+/**
+ * 
+ * @author freddy
+ * @see java.awt.event.ActionListener
+ */
+public class ComoyComo extends JFrame implements ActionListener{
 	private static final long serialVersionUID = 1L;
 	private JTabbedPane pestañas;
 	private JMenuBar menuBar;
 	private JMenu file, edit, help;
-	private JMenuItem documentation;
+	private JMenuItem documentation, exit;
 	
-	public ComoyComo(){
-		super("COMO&COMO: Bienvenido");
+	/**
+	 * @author freddy
+	 * @param windowName
+	 * Constructor.
+	 * Recibe un String que será usado para poner el nombre de la ventana.
+	 */
+	public ComoyComo(String windowName){
+		super(windowName);
 		
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(800,500);
@@ -28,46 +45,73 @@ public class ComoyComo extends JFrame{
 	}
 	
 	private void definePestañas(){
-		// Definición de pestaña de Clientes
-		JPanel pClientes = new JPanel(null);	
-		JLabel lbClientes = new JLabel("Altas, bajas y Cambios");
-		lbClientes.setBounds(100,100,200,70);
+		// Definición de pestaña de CLIENTES.
+		JPanel pClientes = new JPanel(null);
+		// Se define un label para indicar un mensaje antes de la tabla.
+		JLabel lbClientes = new JLabel("Clientes registrados en COMO&COMO");
+		lbClientes.setBounds(10,10,300,20);
 		pClientes.add(lbClientes);
+		// Se define un elemento JSeparator para separar el mensaje anterior de la tabla de Clientes.
+		JSeparator spClientes = new JSeparator();
+		spClientes.setBounds(10,30,300,20);
+		pClientes.add(spClientes);
+		// Se asigna un color al panel y se agrega a la pestaña que lo va a contener.
+		pClientes.setBackground(Color.LIGHT_GRAY);
 		pestañas.addTab("Clientes",null,pClientes);
 		
-		// Definición de pestaña de Platillos
+		// Definición de pestaña de PLATILLOS.
 		JPanel pPlatillos = new JPanel(null);
-		JLabel lbPlatillos = new JLabel("Altas, bajas y Cambios");
-		pPlatillos.setLayout(new BorderLayout());
-		pPlatillos.add(lbPlatillos, BorderLayout.CENTER);
+		JLabel lbPlatillos = new JLabel("Platillos registrados en COMO&COMO");
+		lbPlatillos.setBounds(10,10,300,20);
+		pPlatillos.add(lbPlatillos);
+		JSeparator spPlatillos = new JSeparator();
+		spPlatillos.setBounds(10,30,300,20);
+		pPlatillos.add(spPlatillos);
+		pPlatillos.setBackground(Color.LIGHT_GRAY);
 		pestañas.addTab("Platillos", null, pPlatillos);
 		
-		// Definición de pestaña de Población
+		// Definición de pestaña de POBLACIÓN.
 		JPanel pPoblacion = new JPanel(null);
-		JLabel lbPoblacion = new JLabel("Altas, bajas y Cambios");
-		pPoblacion.setLayout(new BorderLayout());
-		pPoblacion.add(lbPoblacion,BorderLayout.CENTER);
+		JLabel lbPoblacion = new JLabel("Poblaciones registradas en COMO&COMO");
+		lbPoblacion.setBounds(10,10,300,20);
+		pPoblacion.add(lbPoblacion);
+		JSeparator spPoblacion = new JSeparator();
+		spPoblacion.setBounds(10,30,300,20);
+		pPoblacion.add(spPoblacion);
+		pPoblacion.setBackground(Color.LIGHT_GRAY);
 		pestañas.addTab("Población", null, pPoblacion);
 		
-		// Definición de pestaña de Centro
+		// Definición de pestaña de CENTRO.
 		JPanel pCentro = new JPanel(null);
-		JLabel lbCentro = new JLabel("Altas, bajas y Cambios");
-		pCentro.setLayout(new BorderLayout());
-		pCentro.add(lbCentro,BorderLayout.CENTER);
+		JLabel lbCentro = new JLabel("Centros registrados en COMO&COMO");
+		lbCentro.setBounds(10,10,300,20);
+		pCentro.add(lbCentro);
+		JSeparator spCentro = new JSeparator();
+		spCentro.setBounds(10,30,300,20);
+		pCentro.add(spCentro);
+		pCentro.setBackground(Color.LIGHT_GRAY);
 		pestañas.addTab("Centros", null, pCentro);
 		
-		// Definición de pestaña de Empleado
+		// Definición de pestaña de EMPLEADO.
 		JPanel pEmpleado = new JPanel(null);
-		JLabel lbEmpleado = new JLabel("Altas, bajas y Cambios");
-		pEmpleado.setLayout(new BorderLayout());
-		pEmpleado.add(lbEmpleado,BorderLayout.CENTER);
+		JLabel lbEmpleado = new JLabel("Empleados registrados en COMO&COMO");
+		lbEmpleado.setBounds(10,10,300,20);
+		pEmpleado.add(lbEmpleado);
+		JSeparator spEmpleado = new JSeparator();
+		spEmpleado.setBounds(10,30,300,20);
+		pEmpleado.add(spEmpleado);
+		pEmpleado.setBackground(Color.LIGHT_GRAY);
 		pestañas.addTab("Empleados", null, pEmpleado);
 		
-		// Definición de pestaña de Factura
+		// Definición de pestaña de FACTURA.
 		JPanel pFactura = new JPanel(null);
-		JLabel lbFactura= new JLabel("Altas, bajas y Cambios");
-		pFactura.setLayout(new BorderLayout());
-		pFactura.add(lbFactura,BorderLayout.CENTER);
+		JLabel lbFactura = new JLabel("Facturas registradas en COMO&COMO");
+		lbFactura.setBounds(10,10,300,20);
+		pFactura.add(lbFactura);
+		JSeparator spFactura = new JSeparator();
+		spFactura.setBounds(10,30,300,20);
+		pFactura.add(spFactura);
+		pFactura.setBackground(Color.LIGHT_GRAY);
 		pestañas.addTab("Facturas", null, pFactura);
 	}
 	
@@ -77,9 +121,31 @@ public class ComoyComo extends JFrame{
 		edit =  new JMenu("Editar");
 		help = new JMenu("Ayuda");
 		documentation = new JMenuItem("Ver documentación");
+		documentation.addActionListener(this); // Se define el evento del botón de documentación.
+		exit = new JMenuItem("Salir");
+		exit.addActionListener(this); 	// Se define el evento del botón de salida.
 		help.add(documentation);
+		file.add(exit);
 		menuBar.add(file);
 		menuBar.add(edit);
 		menuBar.add(help);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// Se presionó la tecla de Documentación
+		if(e.getSource()==documentation){
+			try {
+				Desktop.getDesktop().open(new File("files/project2.pdf"));
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+		// Se presionó la tecla de Salir.
+		if(e.getSource()==exit){
+			JOptionPane.showMessageDialog(null, "Usted está saliendo de COMO&COMO\nClic en Aceptar para salir.",
+												"Buen día",JOptionPane.INFORMATION_MESSAGE);
+			System.exit(0);
+		}
 	}
 }
